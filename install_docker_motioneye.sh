47dd0ba35023166991e1f5eb12cc2e72fbe64412
halata83 <hali.lucky@gmail.com>
2022-11-04 06:57:02 +0100

    Update install_docker_motioneye.sh
    upraven docker image a mapovani video zarizeni pro HW akceleraci

--- install_docker_motioneye.sh
+++ install_docker_motioneye.sh
@@ -11,4 +11,4 @@ sudo usermod -aG docker lukas
 echo "stahuju docker image motioneye"
 docker pull halilucky/raspi4motioneye:v1
 echo "spoustim docker image"
-docker run -d --name="motioneye" -p 8765:8765 --hostname="motioneye" -v /etc/localtime:/etc/localtime:ro -v /media/motioneye/config:/etc/motioneye -v /media/motioneye/records:/var/lib/motioneye --restart="always" halilucky/raspi4motioneye:v1
+docker run -d --name="motioneye" -p 8765:8765 --hostname="motioneye" --device=/dev/video10 --device=/dev/video11 --device=/dev/video12 --device=/dev/video18 -v /etc/localtime:/etc/localtime:ro -v /media/motioneye/config:/etc/motioneye -v /media/motioneye/records:/var/lib/motioneye --restart="always" halilucky/raspi4motioneye:arm64_H264_v4l2m2m
